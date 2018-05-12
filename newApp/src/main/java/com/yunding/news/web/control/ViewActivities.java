4804cce338059954825815c12a547a5ed5227200
HJY <729179016@qq.com>
2018-05-12 12:06:45 +0800

    5.11Update：功能合并至朋友圈方法
    Signed-off-by: HJY <729179016@qq.com>

--- newApp/src/main/java/com/yunding/news/web/control/ViewActivities.java
+++ newApp/src/main/java/com/yunding/news/web/control/ViewActivities.java
@@ -1,31 +1,24 @@
 package com.yunding.news.web.control;
 
-import com.yunding.news.model.pojo.Account;
-import com.yunding.news.model.service.ServiceFactory;
-import net.sf.json.JSONObject;
-
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-/*
- * 查看关注用户的动态
- *@Author HJY
- */
-@WebServlet(name = "ViewActivities", urlPatterns = {"/main/java/com.yunding.news/web/control/ViewActivities"})
-public class ViewActivities extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) {
-        String JSON = request.getParameter("ViewActivities");
-        JSONObject vd = new JSONObject();
-        vd.getString("");
-        String name = "";
-        Account account = new Account();
-        account = (Account) ServiceFactory.getService("user").findByUserName("username");
-
-    }
-
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) {
-        doPost(request, response);
-    }
-}
+///*
+// * 查看关注用户的动态
+// *@Author HJY
+// *
+// * 5.11Update：功能合并至朋友圈方法
+// */
+//@WebServlet(name = "ViewActivities", urlPatterns = {"/main/java/com.yunding.news/web/control/ViewActivities"})
+//public class ViewActivities extends HttpServlet {
+//    protected void doPost(HttpServletRequest request, HttpServletResponse response) {
+//        String JSON = request.getParameter("ViewActivities");
+//        JSONObject vd = new JSONObject();
+//        vd.getString("");
+//        String name = "";
+//        Account account = new Account();
+//        account = (Account) ServiceFactory.getService("user").findByUserName("username");
+//
+//    }
+//
+//    protected void doGet(HttpServletRequest request, HttpServletResponse response) {
+//        doPost(request, response);
+//    }
+//}
